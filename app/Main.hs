ad6cd733901cd7c9ae51ffe552859ea9cf483278
freedomhero <freedomhero2008@gmail.com>
2021-12-15 22:44:46 +0800

    adjust #transpile function's return type

--- app/Main.hs
+++ app/Main.hs
@@ -1,19 +1,20 @@
 {-# LANGUAGE ScopedTypeVariables #-}
+
 module Main where
 
-import Transpiler
 import Intermediate
 import Scrypt
 import Solidity
+import Transpiler
+
+
 
 main :: IO ()
 main = do
   let solidityCode1 = "uint256"
-  (scryptCode1, _ :: TranspilePath TypeName IType' (Maybe Type)) <- transpile solidityCode1
-  putStrLn $ "Transpile `" ++ solidityCode1 ++ "` to `" ++ scryptCode1 ++ "`"
-  
+  tr1 :: TranspileResult TypeName IType' (Maybe Type) <- transpile solidityCode1
+  putStrLn $ "Transpile `" ++ solidityCode1 ++ "` to `" ++ scryptCode tr1 ++ "`"
+
   let solidityCode2 = "0x123a"
-  (scryptCode2, _ :: TranspilePath Expression IExpr' (Maybe (Expr IExpr))) <- transpile solidityCode2
-  putStrLn $ "Transpile `" ++ solidityCode2 ++ "` to `" ++ scryptCode2 ++ "`"
-
-
+  tr2 :: TranspileResult Expression IExpr' (Maybe (Expr IExpr)) <- transpile solidityCode2
+  putStrLn $ "Transpile `" ++ solidityCode2 ++ "` to `" ++ scryptCode tr2 ++ "`"
--- src/Transpiler.hs
+++ src/Transpiler.hs
@@ -13,11 +13,13 @@ import Solidity
 -- Used in function `transpile` to provide types information
 data TranspilePath sol intm scr = TranspilePath sol intm scr
 
-transpile :: (Parseable a, ToIntermediateTransformable a b, ToScryptTransformable b c, Generable c) => String -> IO (String, TranspilePath a b c)
+data TranspileResult a b c = TranspileResult {scryptCode :: String, tranpilePath :: TranspilePath a b c}
+
+transpile :: (Parseable a, ToIntermediateTransformable a b, ToScryptTransformable b c, Generable c) => String -> IO (TranspileResult a b c)
 transpile solidityCode = do
   sol :: a <- parseIO solidityCode
   itmd :: b <- transform2Intermediate TransformState sol
   scr :: c <- transform2Scrypt itmd
-  scryptCode <- generateScrypt scr
-  -- the purpose of return the second item (i.e. TranspilePath) is to provide type value of a, b, c to Haskell compiler
-  return (scryptCode, TranspilePath sol itmd scr)
+  code <- generateScrypt scr
+  -- the purpose of including `TranspilePath` is to provide the type values of `a`, `b`, `c` to Haskell compiler
+  return $ TranspileResult code $ TranspilePath sol itmd scr
