0b49e49aec43158ee60a2155cd697ce8a33268f7
hh <hf.zheng@scrypt.io>
2021-12-18 13:50:13 +0800

    add BytesLiteral test for Generable

--- test/golden/Generables/Expr.hs
+++ test/golden/Generables/Expr.hs
@@ -17,6 +17,9 @@ spec = testSpec "instance Generable (Expr a)" $ do
     genCode (Just $ Scr.IntLiteral False 15 Nothing) `shouldBe` "15"
     genCode (Just $ Scr.IntLiteral True 32 Nothing) `shouldBe` "0x20"
 
+  it "should generate sCrypt code for `BytesLiteral` correctly" $ do
+    genCode (Just $ Scr.BytesLiteral [1,1,19] Nothing) `shouldBe` "b'010113'"
+
   it "should generate sCrypt code for `UnaryExpr` correctly"  $ do
     genCode (Just $ Scr.UnaryExpr Negate (Scr.IntLiteral False 15 Nothing) Nothing) `shouldBe` "-15"
     genCode (Just $ Scr.UnaryExpr Negate (Scr.IntLiteral True 15 Nothing) Nothing) `shouldBe` "-(0xf)"
