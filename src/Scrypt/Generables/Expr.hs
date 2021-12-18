e5ed66378b83b3d112700ab6cf2fb4c1294865b4
hh <hf.zheng@scrypt.io>
2021-12-18 13:45:27 +0800

    Fix generate code for   it "should generate s

--- src/Scrypt/Generables/Expr.hs
+++ src/Scrypt/Generables/Expr.hs
@@ -17,6 +17,6 @@ instance Generable (Maybe (Scr.Expr a)) where
   genCode (Just (Scr.BytesLiteral b _)) = "b'" ++ concatMap showHexWithPadded b ++ "'"
   genCode (Just (Scr.UnaryExpr Scr.Negate (Scr.IntLiteral _isHex i _) _)) = "-" ++ if _isHex then showHex_ else showInt_
     where
-      showHex_ = "0x" ++ showHex i ""
+      showHex_ = "(0x" ++ showHex i "" ++ ")"
       showInt_ = showInt i ""
   genCode _ = error "unimplemented show scrypt expr"
