03110d0197e36eaa98dcdcdcb9280450a165bb25
hh <hf.zheng@scrypt.io>
2021-12-23 16:18:31 +0800

    IR: export Identifier

--- src/IR/Transformer.hs
+++ src/IR/Transformer.hs
@@ -9,8 +9,10 @@ module IR.Transformer
     module IR.Transformations.Sol2IR.Expression,
     module IR.Transformations.Sol2IR.Statement,
     module IR.Transformations.Sol2IR.Contract,
+    module IR.Transformations.Sol2IR.Identifier,
     module IR.Transformations.IR2Scr.Expression,
     module IR.Transformations.IR2Scr.Statement,
+    module IR.Transformations.IR2Scr.Identifier,
   )
 where
 
@@ -19,10 +21,12 @@ import IR.Transformations.Base
 import IR.Transformations.IR2Scr.Expression
 import IR.Transformations.IR2Scr.Statement
 import IR.Transformations.IR2Scr.Contract
+import IR.Transformations.IR2Scr.Identifier
 import IR.Transformations.IR2Scr.Type
 import IR.Transformations.Sol2IR.Expression
 import IR.Transformations.Sol2IR.Statement
 import IR.Transformations.Sol2IR.Contract
+import IR.Transformations.Sol2IR.Identifier
 import IR.Transformations.Sol2IR.Type ()
 
 class Node a where
