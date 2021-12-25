4f238a9c8998180cc7414f1aa5b056ccccb9c39e
hh <hf.zheng@scrypt.io>
2021-12-25 17:43:22 +0800

    stmt: throw as soon as posible

--- src/IR/Transformations/Sol2IR/Statement.hs
+++ src/IR/Transformations/Sol2IR/Statement.hs
@@ -5,12 +5,12 @@
 module IR.Transformations.Sol2IR.Statement where
 
 import IR.Transformations.Base
-import IR.Transformations.Sol2IR.Expression 
+import IR.Transformations.Sol2IR.Expression
 import Solidity.Spec as Sol
 import IR.Spec as IR
 import IR.Transformations.Sol2IR.Type ()
 import Protolude.Functor
-import Data.Maybe (catMaybes)
+import Data.Maybe (fromJust)
 
 
 instance ToIRTransformable VariableDeclaration IParam' where
@@ -21,14 +21,14 @@ instance ToIRTransformable VariableDeclaration IParam' where
 
 instance ToIRTransformable Sol.Statement IStatement' where
   _toIR (SimpleStatementExpression e) = ExprStmt <<$>> _toIR e
-  _toIR (SimpleStatementVariableAssignmentList ids es) = do
-    ids' <- mapM _toIR $ catMaybes ids
-    es' <- mapM _toIR es
-    let s = AssignStmt ids' $ catMaybes es'
-    return $ Just s
-  _toIR (SimpleStatementVariableDeclarationList declares es) = do
-    declares' <- mapM _toIR $ catMaybes declares
-    es' <- mapM _toIR es
-    let s = DeclareStmt declares' $ catMaybes es'
-    return $ Just s
+  _toIR (SimpleStatementVariableAssignmentList [Just idtf] [e]) = do
+    e' <- _toIR e
+    idtf' <- _toIR idtf
+    return $ Just $ AssignStmt [idtf'] [fromJust e']
+  _toIR (SimpleStatementVariableDeclarationList [Just declare] [e]) = do
+    e' <- _toIR e
+    declare' <- _toIR declare
+    return $ Just $ DeclareStmt [declare'] [fromJust e']
+  _toIR (SimpleStatementVariableAssignmentList ids es) = error "unsupported SimpleStatementVariableAssignmentList"
+  _toIR (SimpleStatementVariableDeclarationList declares es) = error "unsupported SimpleStatementVariableDeclarationList"
   _toIR _ = return Nothing
