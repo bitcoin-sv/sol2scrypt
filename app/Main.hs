dc71ef6a6fc942e9f3072dd3d44436d1e0b769f1
hh <hh@scrypt.io>
2022-02-26 14:56:30 +0800

    add test in main

--- app/Main.hs
+++ app/Main.hs
@@ -1,5 +1,5 @@
 {-# LANGUAGE ScopedTypeVariables #-}
-
+{-# LANGUAGE QuasiQuotes #-}
 module Main where
 
 import IR
@@ -7,31 +7,71 @@ import Scrypt as Scr
 import Solidity as Sol
 import Transpiler
 import Utils
-
+import Text.RawString.QQ
 
 main :: IO ()
 main = do
 
-  let solidityCode1 = "uint256"
-  tr1 :: TranspileResult (TypeName' SourceRange) IType' (Maybe Type) <- transpile solidityCode1
-  putStrLn $ "Transpile `" ++ solidityCode1 ++ "` to `" ++ scryptCode tr1 ++ "`"
+  -- let solidityCode1 = "uint256"
+  -- tr1 :: TranspileResult (TypeName' SourceRange) IType' (Maybe Type) <- transpile solidityCode1
+  -- putStrLn $ "Transpile `" ++ solidityCode1 ++ "` to `" ++ scryptCode tr1 ++ "`"
 
-  let solidityCode2 = "0x123a"
-  tr2 :: TranspileResult Expression IExpression' (Maybe (Expr Ann)) <- transpile solidityCode2
-  putStrLn $ "Transpile `" ++ solidityCode2 ++ "` to `" ++ scryptCode tr2 ++ "`"
-
-
-  let solidityCode = "function set(uint x) external { x; }"
-  f :: ContractPart <- parseIO solidityCode
-  print f
-
-  let solidityCode3 = "function set(uint x) external { 1; }"
-  tr3 :: TranspileResult ContractPart IFunction' (Maybe (Scr.Function Ann)) <- transpile solidityCode3
-  putStrLn $ "Transpile `" ++ solidityCode3 ++ "` to `" ++ scryptCode tr3 ++ "`"
+  -- let solidityCode2 = "0x123a"
+  -- tr2 :: TranspileResult Expression IExpression' (Maybe (Expr Ann)) <- transpile solidityCode2
+  -- putStrLn $ "Transpile `" ++ solidityCode2 ++ "` to `" ++ scryptCode tr2 ++ "`"
 
 
+  -- let solidityCode = "function set(uint x) external { x; }"
+  -- f :: ContractPart <- parseIO solidityCode
+  -- print f
 
-  let solidityCode4 = "uint256[2] storedData;"
-  e :: TypeName' SourceRange <- parseIO solidityCode4
+  -- let solidityCode3 = "function set(uint x) external { 1; }"
+  -- tr3 :: TranspileResult ContractPart IFunction' (Maybe (Scr.Function Ann)) <- transpile solidityCode3
+  -- putStrLn $ "Transpile `" ++ solidityCode3 ++ "` to `" ++ scryptCode tr3 ++ "`"
+
+
+
+  -- let solidityCode4 = "uint256[2] storedData;"
+  -- e :: TypeName' SourceRange <- parseIO solidityCode4
+  -- print e
+
+
+  let solidityCode5 = [r|
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.10;
+
+import "./myLovelyLovelyLib.sol";
+import "ccc.sol";
+
+contract SimpleStorage {
+    uint storedData;
+
+    constructor() {}
+}
+
+contract flipper {
+    bool private value;
+
+    /// Constructor that initializes the `bool` value to the given `init_value`.
+    constructor(bool initvalue) {
+        value = initvalue;
+    }
+
+    /// A message that can be called on instantiated contracts.
+    /// This one flips the value of the stored `bool` from `true`
+    /// to `false` and vice versa.
+    function flip() public {
+        require(value, "an error message");
+        value = !value;
+    }
+
+    /// Simply returns the current value of our `bool`.
+    function get() public view returns (bool) {
+        return value;
+    }
+}
+|]
+
+  e :: SolidityCode' SourceRange <- parseIO solidityCode5
   print e
 
