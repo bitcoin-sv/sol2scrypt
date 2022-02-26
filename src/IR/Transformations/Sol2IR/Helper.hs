6422b108dd9144bfaeb75d3047eb69e9407297e6
hh <hh@scrypt.io>
2022-02-26 21:02:54 +0800

    fix helper.hs

--- src/IR/Transformations/Sol2IR/Helper.hs
+++ src/IR/Transformations/Sol2IR/Helper.hs
@@ -2,77 +2,40 @@ module IR.Transformations.Sol2IR.Helper where
 
 import Solidity
 
-exprExistsInStmt :: Expression -> Statement -> Bool
-exprExistsInStmt e (BlockStatement (Block stmts)) = foldr ((||) . exprExistsInStmt e) False stmts
-exprExistsInStmt e (IfStatement cond trueBranch maybeFalseBranch) =
+exprExistsInStmt :: Expression SourceRange -> Statement SourceRange -> Bool
+exprExistsInStmt e (BlockStatement (Block stmts _)) = foldr ((||) . exprExistsInStmt e) False stmts
+exprExistsInStmt e (IfStatement cond trueBranch maybeFalseBranch _) =
   exprExistsInExpr e cond
     || exprExistsInStmt e trueBranch
     || case maybeFalseBranch of
       Nothing -> False
       Just fb -> exprExistsInStmt e fb
-exprExistsInStmt e (SimpleStatementExpression e') = exprExistsInExpr e e'
-exprExistsInStmt e (SimpleStatementVariableDeclarationList _ rhs) = foldr ((||) . exprExistsInExpr e) False rhs
-exprExistsInStmt e (SimpleStatementVariableAssignmentList _ rhs) = foldr ((||) . exprExistsInExpr e) False rhs
+exprExistsInStmt e (SimpleStatementExpression e' _) = exprExistsInExpr e e'
+exprExistsInStmt e (SimpleStatementVariableDeclarationList _ rhs _) = foldr ((||) . exprExistsInExpr e) False rhs
+exprExistsInStmt e (SimpleStatementVariableAssignmentList _ rhs _) = foldr ((||) . exprExistsInExpr e) False rhs
 exprExistsInStmt _ _ = False
 
-exprExistsInStmt' :: Expression' SourceRange -> Statement' SourceRange -> Bool
-exprExistsInStmt' e (BlockStatement' (Block' stmts _)) = foldr ((||) . exprExistsInStmt' e) False stmts
-exprExistsInStmt' e (IfStatement' cond trueBranch maybeFalseBranch _) =
-  exprExistsInExpr' e cond
-    || exprExistsInStmt' e trueBranch
-    || case maybeFalseBranch of
-      Nothing -> False
-      Just fb -> exprExistsInStmt' e fb
-exprExistsInStmt' e (SimpleStatementExpression' e' _) = exprExistsInExpr' e e'
-exprExistsInStmt' e (SimpleStatementVariableDeclarationList' _ rhs _) = foldr ((||) . exprExistsInExpr' e) False rhs
-exprExistsInStmt' e (SimpleStatementVariableAssignmentList' _ rhs _) = foldr ((||) . exprExistsInExpr' e) False rhs
-exprExistsInStmt' _ _ = False
 
-exprExistsInExpr :: Expression -> Expression -> Bool
-exprExistsInExpr e e'@(Unary _ ue) = e == e' || exprExistsInExprEx e ue
-exprExistsInExpr e e'@(Binary _ lhs rhs) = e == e' || exprExistsInExprEx e lhs || exprExistsInExprEx e rhs
-exprExistsInExpr e e'@(Ternary _ cond lhs rhs) = e == e' || exprExistsInExprEx e cond || exprExistsInExprEx e lhs || exprExistsInExprEx e rhs
-exprExistsInExpr e e'@(FunctionCallNameValueList f pl) =
+exprExistsInExpr :: Expression SourceRange -> Expression SourceRange -> Bool
+exprExistsInExpr e e'@(Unary _ ue _) = e == e' || exprExistsInExprEx e ue
+exprExistsInExpr e e'@(Binary _ lhs rhs _) = e == e' || exprExistsInExprEx e lhs || exprExistsInExprEx e rhs
+exprExistsInExpr e e'@(Ternary _ cond lhs rhs _) = e == e' || exprExistsInExprEx e cond || exprExistsInExprEx e lhs || exprExistsInExprEx e rhs
+exprExistsInExpr e e'@(FunctionCallNameValueList f pl _) =
    e == e' || exprExistsInExprEx e f || case pl of
     Nothing -> False
     Just (NameValueList ns) -> foldr ((||) . exprExistsInExpr e . snd) False ns
-exprExistsInExpr e e'@(FunctionCallExpressionList f pl) =
+exprExistsInExpr e e'@(FunctionCallExpressionList f pl _) =
    e == e' || exprExistsInExprEx e f || case pl of
     Nothing -> False
     Just (ExpressionList ns) -> foldr ((||) . exprExistsInExpr e) False ns
-exprExistsInExpr e e'@(MemberAccess me _) = e == e' || exprExistsInExprEx e me
+exprExistsInExpr e e'@(MemberAccess me _ _) = e == e' || exprExistsInExprEx e me
 exprExistsInExpr e e'@(Literal l) =
    e == e' || case l of
-    PrimaryExpressionTupleExpression (RoundBrackets es) -> foldr ((||) . exprExistsInExpr e) False es
-    PrimaryExpressionTupleExpression (SquareBrackets es) -> foldr ((||) . exprExistsInExpr e) False es
+    PrimaryExpressionTupleExpression (RoundBrackets es _) -> foldr ((||) . exprExistsInExpr e) False es
+    PrimaryExpressionTupleExpression (SquareBrackets es _) -> foldr ((||) . exprExistsInExpr e) False es
     _ -> False
 exprExistsInExpr e e' = e == e'
 
 
-
-exprExistsInExpr' :: Expression' SourceRange -> Expression' SourceRange -> Bool
-exprExistsInExpr' e e'@(Unary' _ ue _) = e == e' || exprExistsInExprEx' e ue
-exprExistsInExpr' e e'@(Binary' _ lhs rhs _) = e == e' || exprExistsInExprEx' e lhs || exprExistsInExprEx' e rhs
-exprExistsInExpr' e e'@(Ternary' _ cond lhs rhs _) = e == e' || exprExistsInExprEx' e cond || exprExistsInExprEx' e lhs || exprExistsInExprEx' e rhs
-exprExistsInExpr' e e'@(FunctionCallNameValueList' f pl _) =
-   e == e' || exprExistsInExprEx' e f || case pl of
-    Nothing -> False
-    Just (NameValueList' ns) -> foldr ((||) . exprExistsInExpr' e . snd) False ns
-exprExistsInExpr' e e'@(FunctionCallExpressionList' f pl _) =
-   e == e' || exprExistsInExprEx' e f || case pl of
-    Nothing -> False
-    Just (ExpressionList' ns) -> foldr ((||) . exprExistsInExpr' e) False ns
-exprExistsInExpr' e e'@(MemberAccess' me _ _) = e == e' || exprExistsInExprEx' e me
-exprExistsInExpr' e e'@(Literal' l) =
-   e == e' || case l of
-    PrimaryExpressionTupleExpression' (RoundBrackets' es _) -> foldr ((||) . exprExistsInExpr' e) False es
-    PrimaryExpressionTupleExpression' (SquareBrackets' es _) -> foldr ((||) . exprExistsInExpr' e) False es
-    _ -> False
-exprExistsInExpr' e e' = e == e'
-
-
-exprExistsInExprEx :: Expression -> Expression -> Bool
+exprExistsInExprEx :: Expression SourceRange -> Expression SourceRange -> Bool
 exprExistsInExprEx decExpr ancExpr = decExpr == ancExpr || exprExistsInExpr decExpr ancExpr
-
-exprExistsInExprEx' :: Expression' SourceRange -> Expression' SourceRange -> Bool
-exprExistsInExprEx' decExpr ancExpr = decExpr == ancExpr || exprExistsInExpr' decExpr ancExpr
