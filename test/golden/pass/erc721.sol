4ac41cea87da26ef9c343d7119627d5c3519e3d9
hh <hh@scrypt.io>
2022-03-16 14:11:18 +0800

    update erc721

--- test/golden/pass/erc721.sol
+++ test/golden/pass/erc721.sol
@@ -2,203 +2,94 @@
 pragma solidity ^0.8.10;
 
 contract ERC721 {
-    //using Address for address;
+    string private constant tokenName = "My ERC721 Token";
+    string private constant tokenSymbol = "MET";
+    uint256 private constant totalTokens = 1000000;
+    mapping(address => uint256) private balances;
+    mapping(uint256 => address) private tokenOwners;
+    mapping(uint256 => bool) private tokenExists;
+    mapping(address => mapping(address => uint256)) private allowed;
+    mapping(address => mapping(uint256 => uint256)) private ownerTokens;
 
-    event Transfer(address indexed from, address indexed to, uint indexed tokenId);
-    event Approval(
-        address indexed owner,
-        address indexed approved,
-        uint indexed tokenId
-    );
-    event ApprovalForAll(
-        address indexed owner,
-        address indexed operator,
-        bool approved
-    );
+    mapping(uint256 => string) tokenLinks;
 
-    // Mapping from token ID to owner address
-    mapping(uint => address) private _owners;
-
-    // Mapping owner address to token count
-    mapping(address => uint) private _balances;
-
-    // Mapping from token ID to approved address
-    mapping(uint => address) private _tokenApprovals;
-
-    // Mapping from owner to operator approvals
-    mapping(address => mapping(address => bool)) private _operatorApprovals;
-
-    function balanceOf(address owner) external view returns (uint) {
-        require(owner != address(0), "owner = zero address");
-        return _balances[owner];
+    function name() external returns (string memory) {
+        return tokenName;
     }
 
-    function ownerOf(uint tokenId) external view  returns (address owner) {
-        owner = _owners[tokenId];
-        require(owner != address(0), "token doesn't exist");
+    function symbol() external returns (string memory) {
+        return tokenSymbol;
     }
 
-    function isApprovedForAll(address owner, address operator)
+    function totalSupply() external returns (uint256) {
+        return totalTokens;
+    }
+
+    function balanceOf(address _owner) external returns (uint256) {
+        return balances[_owner];
+    }
+
+
+    function approve(address _to, uint256 _tokenId) external {
+        require(tokenExists[_tokenId]);
+        require(msg.sender == tokenOwners[_tokenId]);
+        require(msg.sender != _to);
+        allowed[msg.sender][_to] = _tokenId;
+        emit Approval(msg.sender, _to, _tokenId);
+    }
+
+    function takeOwnership(uint256 _tokenId) external {
+        require(tokenExists[_tokenId]);
+        address oldOwner = tokenOwners[_tokenId];
+        address newOwner = msg.sender;
+        require(newOwner != oldOwner);
+        require(allowed[oldOwner][newOwner] == _tokenId);
+        balances[oldOwner] -= 1;
+        tokenOwners[_tokenId] = newOwner;
+        balances[oldOwner] += 1;
+        emit Transfer(oldOwner, newOwner, _tokenId);
+    }
+
+    function transfer(address _to, uint256 _tokenId) external {
+        address currentOwner = msg.sender;
+        address newOwner = _to;
+        require(tokenExists[_tokenId]);
+        require(currentOwner == tokenOwners[_tokenId]);
+        require(currentOwner != newOwner);
+        require(newOwner != address(0));
+
+        for (uint256 i = 0; ownerTokens[currentOwner][i] != _tokenId; i++) {
+            ownerTokens[currentOwner][i] = 0;
+        }
+
+        balances[currentOwner] -= 1;
+        tokenOwners[_tokenId] = newOwner;
+        balances[newOwner] += 1;
+        emit Transfer(currentOwner, newOwner, _tokenId);
+    }
+
+    function tokenOfOwnerByIndex(address _owner, uint256 _index)
         external
-        view
-        
-        returns (bool)
+        returns (uint256 tokenId)
     {
-        return _operatorApprovals[owner][operator];
+        return ownerTokens[_owner][_index];
     }
 
-    function setApprovalForAll(address operator, bool approved) external  {
-        _operatorApprovals[msg.sender][operator] = approved;
-        emit ApprovalForAll(msg.sender, operator, approved);
+    function tokenMetadata(uint256 _tokenId)
+        external
+        returns (string memory infoUrl)
+    {
+        return tokenLinks[_tokenId];
     }
 
-    function getApproved(uint tokenId) external view  returns (address) {
-        require(_owners[tokenId] != address(0), "token doesn't exist");
-        return _tokenApprovals[tokenId];
-    }
-
-    // function _approve(
-    //     address owner,
-    //     address to,
-    //     uint tokenId
-    // ) private {
-    //     _tokenApprovals[tokenId] = to;
-    //     emit Approval(owner, to, tokenId);
-    // }
-
-    function approve(address to, uint tokenId) external  {
-        address owner = _owners[tokenId];
-        require(
-            msg.sender == owner || _operatorApprovals[owner][msg.sender],
-            "not owner nor approved for all"
-        );
-        _tokenApprovals[tokenId] = to;
-        emit Approval(owner, to, tokenId);
-    }
-
-    // function _isApprovedOrOwner(
-    //     address owner,
-    //     address spender,
-    //     uint tokenId
-    // ) private view returns (bool) {
-    //     return (spender == owner ||
-    //         _tokenApprovals[tokenId] == spender ||
-    //         _operatorApprovals[owner][spender]);
-    // }
-
-    // function _transfer(
-    //     address owner,
-    //     address from,
-    //     address to,
-    //     uint tokenId
-    // ) private {
-    //     require(from == owner, "not owner");
-    //     require(to != address(0), "transfer to the zero address");
-
-    //     _approve(owner, address(0), tokenId);
-
-    //     _balances[from] -= 1;
-    //     _balances[to] += 1;
-    //     _owners[tokenId] = to;
-
-    //     emit Transfer(from, to, tokenId);
-    // }
-
-    function transferFrom(
-        address from,
-        address to,
-        uint tokenId
-    ) external {
-        address owner = _owners[tokenId];
-        require(owner != address(0), "token doesn't exist");
-
-        require(msg.sender == owner ||
-            _tokenApprovals[tokenId] == msg.sender ||
-            _operatorApprovals[owner][msg.sender], "not owner nor approved");
-
-        // do _transfer
-
-        require(from == owner, "not owner");
-        require(to != address(0), "transfer to the zero address");
-
-        _tokenApprovals[tokenId] = address(0);
-        emit Approval(owner, address(0), tokenId);
-
-        _balances[from] -= 1;
-        _balances[to] += 1;
-        _owners[tokenId] = to;
-
-        emit Transfer(from, to, tokenId);
-
-    }
-
-    // function _safeTransfer(
-    //     address owner,
-    //     address from,
-    //     address to,
-    //     uint tokenId,
-    //     bytes memory _data
-    // ) private {
-    //     _transfer(owner, from, to, tokenId);
-    //     //require(_checkOnERC721Received(from, to, tokenId, _data), "not ERC721Receiver");
-    // }
-
-    function safeTransferFrom(
-        address from,
-        address to,
-        uint tokenId,
-        bytes memory _data
-    ) external {
-        address owner = _owners[tokenId];
-        require(owner != address(0), "token doesn't exist");
-        require((msg.sender == owner ||
-            _tokenApprovals[tokenId] == msg.sender ||
-            _operatorApprovals[owner][msg.sender]), "not owner nor approved");
-        // do _safeTransfer
-        require(from == owner, "not owner");
-        require(to != address(0), "transfer to the zero address");
-
-        _tokenApprovals[tokenId] = address(0);
-        emit Approval(owner, address(0), tokenId);
-
-        _balances[from] -= 1;
-        _balances[to] += 1;
-        _owners[tokenId] = to;
-
-        emit Transfer(from, to, tokenId);
-    }
-
-    // // Todo: https://github.com/sCrypt-Inc/sol2scrypt/issues/155
-    // // function safeTransferFrom(
-    // //     address from,
-    // //     address to,
-    // //     uint tokenId
-    // // ) external {
-    // //     safeTransferFrom(from, to, tokenId, "");
-    // // }
-
-    function mint(address to, uint tokenId) external {
-        require(to != address(0), "mint to zero address");
-        require(_owners[tokenId] == address(0), "token already minted");
-
-        _balances[to] += 1;
-        _owners[tokenId] = to;
-
-        emit Transfer(address(0), to, tokenId);
-    }
-
-    function burn(uint tokenId) external {
-        address owner = _owners[tokenId];
-        require(owner != address(0), "token doesn't exist");
-
-        _tokenApprovals[tokenId] = address(0);
-        emit Approval(owner, address(0), tokenId);
-
-        _balances[owner] -= 1;
-
-        // todo: transpile delete
-        // delete _owners[tokenId];
-
-        emit Transfer(owner, address(0), tokenId);
-    }
+    event Transfer(
+        address indexed _from,
+        address indexed _to,
+        uint256 _tokenId
+    );
+    event Approval(
+        address indexed _owner,
+        address indexed _approved,
+        uint256 _tokenId
+    );
 }
