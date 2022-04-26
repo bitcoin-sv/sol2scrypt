edfae0cddaf094afa26632eaed5044071fae2bb5
hh <hh@scrypt.io>
2022-04-26 16:42:54 +0800

    add immutable property

--- test/golden/pass/SimpleStorage.sol
+++ test/golden/pass/SimpleStorage.sol
@@ -2,7 +2,12 @@ pragma solidity ^0.8.10;
 
 contract SimpleStorage {
     uint storedData;
+ 
+    uint immutable decimals;
 
+    constructor() {
+        decimals = 3;
+    }
     function set(uint x) external {
         storedData = x;
     }
