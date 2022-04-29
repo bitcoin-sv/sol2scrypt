198a7e9902ef83da050c589f00434f93d6c1096d
hh <hh@scrypt.io>
2022-04-29 09:48:54 +0800

    update test

--- test/golden/fail/mapping.sol
+++ test/golden/fail/mapping.sol
@@ -5,8 +5,9 @@ pragma solidity ^0.8.10;
 contract Mapping {
 
     uint i = 11;
-    
+
     mapping(address => uint) public myMap;
+    mapping(uint => address) public map1;
     
     function get(address _addr) external view returns (uint) {
         return myMap[_addr];
@@ -19,4 +20,11 @@ contract Mapping {
     function set(address _addr, uint _i) external {
         myMap[_addr] = _i;
     }
+
+    function update(uint a, address _addr) external {
+        map1[a] = _addr;
+        a++;
+        map1[a] = _addr;
+    }
+
 }
